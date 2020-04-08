8a3cea5836267c6b6bf92db9b925acda2e680710
tusharlimbhore <38065459+tusharlimbhore@users.noreply.github.com>
2020-04-08 22:29:30 +0530

    new workflows added for update servers in pool & delete object (#36)
    New workflows added for update servers in pool & delete object

--- create_release.sh
+++ create_release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 assets=""
-echo "Usage ./create_release.sh <branch> <version> <is_final> <build_number>"
+echo "Usage ./create_release.sh <branch> <version> <build_number> <is_final>"
 REL=$2
 BRANCH=$1
-IS_FINAL=$3
-BUILD_NUMBER=$4
+IS_FINAL=$4
+BUILD_NUMBER=$3
 if [ $BRANCH = "eng" ]; then
     BRANCH="master"
 fi
@@ -23,9 +23,42 @@ fi
 
 if [ $IS_FINAL = true ]; then
     mvn versions:set -DnewVersion=$REL
+    REL_TAG=tag-$REL
 else
 	mvn versions:set -DnewVersion=$REL-beta-$BUILD_NUMBER
+	REL_TAG=tag-$REL\b$BUILD_NUMBER
 fi
+
+echo "Release tag is $REL_TAG"
+
+git tag -d $REL_TAG
+git tag -d $REL
+git tag $REL_TAG
+git push -f origin $REL_TAG
+set -e
+git checkout -B $BRANCH
+
+releases=`/usr/local/bin/hub release`
+hub_op="create"
+for r in $releases
+do
+    if [ "$r" = "$REL_TAG" ]; then
+        hub_op="edit"
+        break
+    fi
+done
+
 mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dbuild.number=$BUILD_NUMBER
 
+if [ $IS_FINAL = true ]; then
+    cp o11nplugin-vro/target/o11nplugin-vro-$REL.dar .
+    assets="$assets -a o11nplugin-vro-$REL.dar"
+else
+	cp o11nplugin-vro/target/o11nplugin-vro-$REL-beta-$BUILD_NUMBER.dar .
+	assets="$assets -a o11nplugin-vro-$REL-beta-$BUILD_NUMBER.dar"
+fi
+
+/usr/local/bin/hub release $hub_op $assets -F ReleaseNote $REL_TAG
+
+rm -rf o11nplugin-vro-*.dar
 
--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/VroPluginFactory.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/VroPluginFactory.java
@@ -1,6 +1,9 @@
 package com.vmware.avi.vro;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.vmware.o11n.plugin.sdk.spring.AbstractSpringPluginFactory;
 import com.vmware.o11n.plugin.sdk.spring.InventoryRef;
@@ -14,6 +17,7 @@ import ch.dunes.vso.sdk.api.QueryResult;
  */
 public final class VroPluginFactory extends AbstractSpringPluginFactory {
 
+	public static Map<String, AviVroClient> aviVroClientMap = new HashMap<String, AviVroClient>();
 	@Override
 	public Object find(InventoryRef ref) {
 		if (ref.isOfType(Constants.FINDER_AVI_VRO_CLIENT)) {
@@ -24,10 +28,15 @@ public final class VroPluginFactory extends AbstractSpringPluginFactory {
 			throw new UnsupportedOperationException("implement me");
 		}
 	}
-
+	/*
+	@Override
+	
+	 * public QueryResult findAll(String type, String query) { throw new
+	 * UnsupportedOperationException("implement me"); }
+	 */
 	@Override
 	public QueryResult findAll(String type, String query) {
-		throw new UnsupportedOperationException("implement me");
+		return new QueryResult(getListOfAviVroClientList());
 	}
 
 	@Override
@@ -38,5 +47,13 @@ public final class VroPluginFactory extends AbstractSpringPluginFactory {
 	@Override
 	public List<?> findChildrenInRelation(InventoryRef parent, String relationName) {
 		throw new UnsupportedOperationException("implement me");
+	}
+	public static List<AviVroClient> getListOfAviVroClientList() {
+		List<AviVroClient> listOfAviVroClient = new ArrayList<AviVroClient>(aviVroClientMap.values());
+		return listOfAviVroClient;
+	}
+
+	public static Map<String, AviVroClient> getListOfAviVroClientMap() {
+		return aviVroClientMap;
 	}
 }
