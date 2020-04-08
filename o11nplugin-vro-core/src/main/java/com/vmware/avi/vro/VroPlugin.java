9ce9e1d1fa06463e291880b81de0128aca3ca11c
tusharlimbhore <tushar.limbhore@avinetworks.com>
2020-04-08 22:03:07 +0530

    new workflows added for update servers in pool & delete object

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/VroPlugin.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/VroPlugin.java
@@ -1,5 +1,7 @@
 package com.vmware.avi.vro;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vmware.avi.sdk.AviCredentials;
@@ -29,5 +31,26 @@ public class VroPlugin {
 		creds.setVersion(version);
 		service.setCred(creds);
 		return service;
+	}
+	@VsoMethod
+	public void addVroClient(String controller, String username, String password, String tenant, String version) {
+
+		AviCredentials aviCredentials = new AviCredentials(controller, username, password);
+		aviCredentials.setTenant(tenant);
+		aviCredentials.setVersion(version);
+		AviVroClient aviVroClient = new AviVroClient();
+		aviVroClient.setCred(aviCredentials);
+		// List<AviVroClient> listOfAviVroClients = VroPluginFactory.listOfAviVroClient;
+		// VroPluginFactory.listOfAviVroClient.add(aviVroClient);
+		
+		VroPluginFactory.aviVroClientMap.put(controller, aviVroClient);
+	}
+
+	@VsoMethod
+	public void removeVroClient(String controller) {
+		Map<String, AviVroClient> aviVroClientsMap = VroPluginFactory.getListOfAviVroClientMap();
+		if (aviVroClientsMap.containsKey(controller)) {
+			aviVroClientsMap.remove(controller);
+		}
 	}
 }
