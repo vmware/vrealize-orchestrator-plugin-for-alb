7957067073eeef195f7507b6656fb0f35e6fe365
mkhachane <khachanem@vmware.com>
2023-05-30 04:46:39 +0000

    AV-179055: Update documentation for AVI vRO plugin.

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/AviVroClient.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/AviVroClient.java
@@ -408,7 +408,7 @@ public class AviVroClient {
 		logger.debug("File Upload Uri : " + fileUploadUri);
 		AviApi session = getSession();
 		if (uri != null && filePath != null && fileUploadUri != null) {
-			session.fileUpload(uri, filePath, fileUploadUri);
+			session.fileUpload(uri, filePath);
 		}
 	}	
 
