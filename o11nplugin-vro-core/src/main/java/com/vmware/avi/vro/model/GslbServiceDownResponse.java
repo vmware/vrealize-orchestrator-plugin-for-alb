8548fd2fa7b12dee17b22091ccf6e886c6328b51
Sagar <ssalvi@vmware.com>
2023-08-24 01:53:34 -0700

    Updated assets for vro
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/GslbServiceDownResponse.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/GslbServiceDownResponse.java
@@ -47,7 +47,7 @@ public class GslbServiceDownResponse extends AviRestResource {
   /**
    * This is the getter method this will return the attribute value.
    * Fallback cname to use in response to the client query when the gslb service is down.
-   * Field introduced in 30.2.1.
+   * Field introduced in 30.2.1, 22.1.5.
    * Allowed in enterprise edition with any value, enterprise with cloud services edition.
    * Default value when not specified in API or module is interpreted by Avi Controller as null.
    * @return fallbackCname
@@ -60,7 +60,7 @@ public class GslbServiceDownResponse extends AviRestResource {
   /**
    * This is the setter method to the attribute.
    * Fallback cname to use in response to the client query when the gslb service is down.
-   * Field introduced in 30.2.1.
+   * Field introduced in 30.2.1, 22.1.5.
    * Allowed in enterprise edition with any value, enterprise with cloud services edition.
    * Default value when not specified in API or module is interpreted by Avi Controller as null.
    * @param fallbackCname set the fallbackCname.
