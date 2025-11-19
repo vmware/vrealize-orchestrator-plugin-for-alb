d3af9fde2325bfd65c371bbd93386b90933c34d9
avijenkins <avi.jenkins@broadcom.com>
2025-11-19 09:59:49 +0530

    Auto updated assets for vro eng (#298)
    * Updated assets for vro
    
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>
    
    * Updated assets for vro
    
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>
    
    * Updated assets for vro
    
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>
    
    ---------
    
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>
    Co-authored-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/ServiceEngineGroup.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/ServiceEngineGroup.java
@@ -7371,10 +7371,9 @@ public class ServiceEngineGroup extends AviRestResource {
 
   /**
    * This is the getter method this will return the attribute value.
-   * This knob enables kernel rss.
-   * When enabled flowtable entry is added to every disp cpu.
+   * Allows runtime updates and planting of ft entries across all dispacther cpu.
+   * Any changes does not impact existing flows.
    * Should be used under supervision.
-   * Requires se reboot.
    * Field introduced in 31.2.1.
    * Allowed with any value in enterprise, enterprise with cloud services edition.
    * Default value when not specified in API or module is interpreted by Avi Controller as false.
@@ -7387,10 +7386,9 @@ public class ServiceEngineGroup extends AviRestResource {
 
   /**
    * This is the setter method to the attribute.
-   * This knob enables kernel rss.
-   * When enabled flowtable entry is added to every disp cpu.
+   * Allows runtime updates and planting of ft entries across all dispacther cpu.
+   * Any changes does not impact existing flows.
    * Should be used under supervision.
-   * Requires se reboot.
    * Field introduced in 31.2.1.
    * Allowed with any value in enterprise, enterprise with cloud services edition.
    * Default value when not specified in API or module is interpreted by Avi Controller as false.
