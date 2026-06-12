1c27349d4f4dd545b1bc8c49293763b9a8bdf738
Mayur Khachane <mayur.khachane@broadcom.com>
2026-06-12 06:05:57 +0000

    Upgraded vRO plugin java version from 8 to 17
    Signed-off-by: Mayur Khachane <mayur.khachane@broadcom.com>

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/TechSupportMessage.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/TechSupportMessage.java
@@ -40,12 +40,7 @@ public class TechSupportMessage extends AviRestResource {
     @JsonInclude(Include.NON_NULL)
     private String url = "url";
 
-    @JsonProperty("uuid")
-    @JsonInclude(Include.NON_NULL)
-    private String uuid;
-
-
-
+   
   /**
    * This is the getter method this will return the attribute value.
    * 'techsupport status for the current invocation.'.
