d9fe9bec475e49bbc18fdd1857484eade136f4a8
avijenkins <svc.avi-jenkins@broadcom.com>
2026-06-29 09:03:40 +0000

    Updated assets for vro
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/MetricsEventThreshold.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/MetricsEventThreshold.java
@@ -43,7 +43,7 @@ public class MetricsEventThreshold extends AviRestResource {
    * Type of the metrics event threshold.
    * This value will decide which metric rule (or rules) use configured thresholds.
    * Enum options - THRESHOLD_TYPE_STATIC, SE_CPU_THRESHOLD, SE_MEM_THRESHOLD, SE_DISK_THRESHOLD, CONTROLLER_CPU_THRESHOLD, CONTROLLER_MEM_THRESHOLD,
-   * CONTROLLER_DISK_THRESHOLD.
+   * CONTROLLER_DISK_THRESHOLD, SE_SHM_MEM_THRESHOLD, SE_CONN_MEM_THRESHOLD.
    * Field introduced in 20.1.3.
    * Allowed with any value in enterprise, enterprise with cloud services edition.
    * Default value when not specified in API or module is interpreted by Avi Controller as null.
@@ -59,7 +59,7 @@ public class MetricsEventThreshold extends AviRestResource {
    * Type of the metrics event threshold.
    * This value will decide which metric rule (or rules) use configured thresholds.
    * Enum options - THRESHOLD_TYPE_STATIC, SE_CPU_THRESHOLD, SE_MEM_THRESHOLD, SE_DISK_THRESHOLD, CONTROLLER_CPU_THRESHOLD, CONTROLLER_MEM_THRESHOLD,
-   * CONTROLLER_DISK_THRESHOLD.
+   * CONTROLLER_DISK_THRESHOLD, SE_SHM_MEM_THRESHOLD, SE_CONN_MEM_THRESHOLD.
    * Field introduced in 20.1.3.
    * Allowed with any value in enterprise, enterprise with cloud services edition.
    * Default value when not specified in API or module is interpreted by Avi Controller as null.
