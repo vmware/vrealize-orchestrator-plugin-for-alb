0622f58b1d9587b757e8b52b4e6f485a6d21cb61
avijenkins <avi.jenkins@broadcom.com>
2026-06-30 11:14:15 +0530

    Updated assets for vro (#335)
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>
    Co-authored-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/TechSupportMessage.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/TechSupportMessage.java
@@ -40,7 +40,8 @@ public class TechSupportMessage extends AviRestResource {
     @JsonInclude(Include.NON_NULL)
     private String url = "url";
 
-   
+
+
   /**
    * This is the getter method this will return the attribute value.
    * 'techsupport status for the current invocation.'.
