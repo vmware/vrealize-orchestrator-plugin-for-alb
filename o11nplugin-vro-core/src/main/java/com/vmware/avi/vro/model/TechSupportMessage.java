d04cfcc5adaaf68c9c9b7b89bb18cd6a7a29c73b
Mayur Khachane <mayur.khachane@broadcom.com>
2026-09-08 12:16:33 +0530

    Added: fix for pass 'name' parameter via query map to prevent double URL-encoding and HTTP 404 errors. (#358)
    Signed-off-by: Mayur Khachane <mayur.khachane@broadcom.com>

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/TechSupportMessage.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/TechSupportMessage.java
@@ -41,7 +41,10 @@ public class TechSupportMessage extends AviRestResource {
     private String url = "url";
 
 
-
+    @JsonProperty("uuid")
+    @JsonInclude(Include.NON_NULL)
+    private String uuid;
+    
   /**
    * This is the getter method this will return the attribute value.
    * 'techsupport status for the current invocation.'.
