ef15ff6f0ac029687143c370068baf3c4964b307
avijenkins <svc.avi-jenkins@broadcom.com>
2026-09-09 09:04:35 +0000

    Updated assets for vro
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/ApiMetricsLimits.java
+++ o11nplugin-vro-core/src/main/java/com/vmware/avi/vro/model/ApiMetricsLimits.java
@@ -24,11 +24,45 @@ import org.springframework.stereotype.Service;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Service
 public class ApiMetricsLimits extends AviRestResource {
+    @JsonProperty("disk_kb_per_endpoint")
+    @JsonInclude(Include.NON_NULL)
+    private Integer diskKbPerEndpoint;
+
     @JsonProperty("num_apis")
     @JsonInclude(Include.NON_NULL)
     private Integer numApis;
 
 
+
+  /**
+   * This is the getter method this will return the attribute value.
+   * Disk space consumed in metrics_db per api endpoint for which metrics are tracked, used to derive num_apis from the disk capacity allocated to
+   * metrics_db.
+   * Field introduced in 32.1.4.
+   * Unit is kb.
+   * Allowed with any value in enterprise, essentials, basic, enterprise with cloud services edition.
+   * Default value when not specified in API or module is interpreted by Avi Controller as null.
+   * @return diskKbPerEndpoint
+   */
+  @VsoMethod
+  public Integer getDiskKbPerEndpoint() {
+    return diskKbPerEndpoint;
+  }
+
+  /**
+   * This is the setter method to the attribute.
+   * Disk space consumed in metrics_db per api endpoint for which metrics are tracked, used to derive num_apis from the disk capacity allocated to
+   * metrics_db.
+   * Field introduced in 32.1.4.
+   * Unit is kb.
+   * Allowed with any value in enterprise, essentials, basic, enterprise with cloud services edition.
+   * Default value when not specified in API or module is interpreted by Avi Controller as null.
+   * @param diskKbPerEndpoint set the diskKbPerEndpoint.
+   */
+  @VsoMethod
+  public void setDiskKbPerEndpoint(Integer  diskKbPerEndpoint) {
+    this.diskKbPerEndpoint = diskKbPerEndpoint;
+  }
 
   /**
    * This is the getter method this will return the attribute value.
@@ -69,14 +103,16 @@ public boolean equals(java.lang.Object o) {
     return false;
   }
   ApiMetricsLimits objApiMetricsLimits = (ApiMetricsLimits) o;
-  return   Objects.equals(this.numApis, objApiMetricsLimits.numApis);
+  return   Objects.equals(this.numApis, objApiMetricsLimits.numApis)&&
+  Objects.equals(this.diskKbPerEndpoint, objApiMetricsLimits.diskKbPerEndpoint);
 }
 
 @Override
 public String toString() {
   StringBuilder sb = new StringBuilder();
   sb.append("class ApiMetricsLimits {\n");
-      sb.append("    numApis: ").append(toIndentedString(numApis)).append("\n");
+      sb.append("    diskKbPerEndpoint: ").append(toIndentedString(diskKbPerEndpoint)).append("\n");
+        sb.append("    numApis: ").append(toIndentedString(numApis)).append("\n");
       sb.append("}");
   return sb.toString();
 }
